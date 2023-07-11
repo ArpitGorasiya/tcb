6d129f92c70c299116c4ea0ec26ab0015128071a
ArpitGorasiya <126314689+ArpitGorasiya@users.noreply.github.com>
2023-07-11 16:39:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors(credentials = true))
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 app.use('/api/v1', course)
 app.use('/api/v1', user)
